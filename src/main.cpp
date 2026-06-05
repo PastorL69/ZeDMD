0f0717ba8bccaf060b287c9033ec922232fec266
Jan Vos <jjcvos@hotmail.nl>
2026-06-05 21:17:58 +0200

    dont check for loopback, this can be disabled in other ways even if dmdreader hasnt initialized.

--- src/main.cpp
+++ src/main.cpp
@@ -2721,7 +2721,7 @@ void loop1() {
   auto *spiTransport = static_cast<SpiTransport *>(transport);
   static uint32_t lastDmdReaderInitAttempt = 0;
 
-  if (!spiTransport->isDmdReaderInitialized() && transport->isLoopback()) {
+  if (!spiTransport->isDmdReaderInitialized()) {
     spiTransport->initDmdReader();
     // Blink to indicate that loopback mode is active but DMD reader is not yet
     // initialized.
@@ -2730,9 +2730,7 @@ void loop1() {
     digitalWrite(LED_BUILTIN, LOW);
     delay(200);
     return;
-  }
-
-  if (!transport->isLoopback()) {
+  } else if (!transport->isLoopback()) {
     dmdreader_spi_send();
     tight_loop_contents();
   } else {
