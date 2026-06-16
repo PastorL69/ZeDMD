931152706b015c406fc834785d50f18c62063145
Jan Vos <jjcvos@hotmail.nl>
2026-06-17 01:20:03 +0200

    change gpio to 21 (ARGB 1)

--- src/pico/ws2812.h
+++ src/pico/ws2812.h
@@ -13,7 +13,7 @@
 #include "hardware/clocks.h"
 #include "main.h"
 
-#define LEFT_SPEAKER_PIN 24
+#define LEFT_SPEAKER_PIN 21
 #define RGBW false
 #define NUM_PIXELS 16
 #define FREQ 800000 // 800khz speed
