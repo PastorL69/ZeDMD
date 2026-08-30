5c1eab4883ef56218b8bc2fc7d4ea9898e84455b
Jan Vos <jjcvos@hotmail.nl>
2026-08-30 14:40:46 +0200

    more robust floating detection. None of these values come close to legit dmdreader values.

--- src/main.cpp
+++ src/main.cpp
@@ -179,8 +179,8 @@ static void DisplayPpucDmdDebug() {
   uint16_t rclk = detected_signals >> 16;  // never exceeds 25000
   uint16_t rdata = detected_signals;       // never exceeds 600
 
-  if (dotclk >= 17170000 && rdata == 9433) {
-    // These are the consistent values when the DMDreader pins are left floating
+  if (dotclk >= 17170000 || rdata >= 9433) {
+    // These are consistent values when the DMDreader pins are left floating
     return;
   }
 
